14163f394feaf5d9db2fd379f54adccf68303ea6
Andres Guerrero <143152842+Andres2003575@users.noreply.github.com>
2025-10-22 01:00:53 -0500

    Version with no docker
    This version worsk without docker

---
+++ usuario/usuario/src/main/java/co/edu/unbosque/usuario/service/UsuarioService.java
@@ -0,0 +1,97 @@
+package co.edu.unbosque.usuario.service;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import co.edu.unbosque.usuario.model.Usuario;
+import co.edu.unbosque.usuario.repository.UsuarioRepository;
+import jakarta.transaction.Transactional;
+
+@Service
+public class UsuarioService {
+
+	@Autowired
+	private UsuarioRepository uRepo;
+
+	@Autowired
+	private PasswordEncoder cifrador;
+
+	public Usuario crearUsuario(String nombreCompleto, String tipoDocumento, String numeroDocumento, String email,
+			String telefono, String contrasena, String estadoCuenta, LocalDate fechaRegistro, double saldo,
+			String rol) {
+
+		String contrasenaCifrada = cifrador.encode(contrasena);
+
+		Usuario u = new Usuario(nombreCompleto, tipoDocumento, numeroDocumento, email, telefono, contrasenaCifrada,
+				estadoCuenta, fechaRegistro, rol);
+
+		return uRepo.save(u);
+	}
+
+	public List<Usuario> findAll() {
+		return uRepo.findAll();
+	}
+
+	public void deleteById(Long id) {
+		uRepo.deleteById(id);
+	}
+
+	public boolean findById(Long id) {
+		return uRepo.existsById(id);
+	}
+
+	@Transactional
+	public Usuario actualizar(Long id, Usuario nuevo) {
+		Usuario u = uRepo.findById(id).orElseThrow(() -> new RuntimeException("Usuario no encontrado"));
+		u.setNombreCompleto(nuevo.getNombreCompleto());
+		u.setTipoDocumento(nuevo.getTipoDocumento());
+		u.setNumeroDocumento(nuevo.getNumeroDocumento());
+		u.setEmail(nuevo.getEmail());
+		u.setTelefono(nuevo.getTelefono());
+		u.setContrasena(cifrador.encode(nuevo.getContrasena()));
+		u.setEstadoCuenta(nuevo.getEstadoCuenta());
+		u.setFechaRegistro(nuevo.getFechaRegistro());
+		u.setRol(nuevo.getRol());
+
+		return uRepo.save(u);
+
+	}
+
+	public boolean login(String email, String contrasena) {
+		for (Usuario u : findAll()) {
+			if (u.getEmail().equals(email)) {
+				if (cifrador.matches(contrasena, u.getContrasena())) {
+					return true;
+				} else {
+					return false;
+				}
+
+			}
+		}
+		return false;
+	}
+	
+	public String iniciarPago(Long id, double monto) {
+		try {
+			RestTemplate restTemplate = new RestTemplate();
+			String urlPagos = "http://localhost:8083/api/pagos/pago?id=" + id + "&amount=" + (long) monto;
+			String urlPagoStripe = restTemplate.postForObject(urlPagos, null, String.class);
+			return urlPagoStripe;
+		} catch (Exception e) {
+			throw new RuntimeException("Error comunicándose con el servicio de pagos: " + e.getMessage());
+		}
+	}
+
+	@Transactional
+	public Usuario recargarSaldo(Long id, double monto) {
+		Usuario usuario = uRepo.findById(id).orElseThrow(() -> new RuntimeException("Usuario no encontrado"));
+		usuario.setSaldo(usuario.getSaldo() + monto);
+		return uRepo.save(usuario);
+	}
+
+}
